c4274c1d8ee78ffd7b73ea6f271d35925348d43d
esteloabell <manitobaaa@gamil.com>
2021-04-13 18:42:54 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,21 @@ function linkAction() {
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll('secion[id]')
 
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id');
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+    }
+  })
+}
 
 /*===== CHANGE BACKGROUND HEADER =====*/
 
