105130da5f3097cc269ee6b781489a22bb203ce0
esteloabell <manitobaaa@gamil.com>
2021-04-14 18:00:58 -0500

    added styles and dummy data

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,3 +97,8 @@ const mySwiper = new Swiper('.testimonial__container', {
 });
 
 /*===== GSAP ANIMATION =====*/
+gsap.from('.home__img', {opacity: 0, duration: 2, delay: .5, X:60})
+gsap.from('.home__data', { opacity: 0, duration: 2, delay: .8, X: 25 })
+gsap.from('.nav__logo, .nav__toggle', { opacity: 0, duration: 2, delay: 1.5, y: 25, ease:'expo.out', stagger: .2})
+gsap.from('.nav__item', { opacity: 0, duration: 2, delay: 1.8, y: 25, ease: 'expo.out', stagger: .2 })
+gsap.from('.home__social-icon', { opacity: 0, duration: 2, delay: 1.8, y: 25, ease: 'expo.out', stagger: .2 })
